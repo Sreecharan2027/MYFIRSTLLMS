197a4083b9883fd8b376d803b74aaf12b5d3be30
Sreecharan2027 <mamidisreecharan20@gmail.com>
2024-06-16 11:16:58 +0530

    nltk

--- lecture_proc_streamlit.py
+++ lecture_proc_streamlit.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import os
 import logging
-import nltk
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import faiss
-
+import nltk
 # Set environment variables to suppress warnings
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 os.environ['HF_HUB_DISABLE_SYMLINKS_WARNING'] = '1'
